312656ae36b9e9717c9a50e2f67567854221e1fe
satyammayy <84136113+satyammayy@users.noreply.github.com>
2021-07-05 07:24:16 +0530

    Update Enable.ts

--- src/commands/Dev/Enable.ts
+++ src/commands/Dev/Enable.ts
@@ -7,7 +7,7 @@ export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
             command: 'enable',
-            description: 'Enables the given command',
+            description: 'Command amuk hanna sijan naba yana bne!',
             category: 'dev',
             usage: `${client.config.prefix}enable [command]`
         })
@@ -16,12 +16,12 @@ export default class Command extends BaseCommand {
     run = async (M: ISimplifiedMessage, { joined }: IParsedArgs): Promise<void> => {
         if (!this.client.config.mods?.includes(M.sender.jid)) return void null
         const key = joined.toLowerCase().trim()
-        if (!key) return void (await M.reply(`Provide the command you want to enable`))
+        if (!key) return void (await M.reply(`Enable twningba command do specify twbirkoh!`))
         const command = this.handler.commands.get(key) || this.handler.aliases.get(key)
-        if (!command) return void (await M.reply(`No command found`))
+        if (!command) return void (await M.reply(`Sumaina twba command leite!`))
         if (!(await this.client.DB.disabledcommands.findOne({ command: command.config.command })))
-            return void M.reply(`${this.client.util.capitalize(command.config.command)} is already enabled`)
+            return void M.reply(`${this.client.util.capitalize(command.config.command)} Sidi Hanna enable twraga leiye!`)
         await this.client.DB.disabledcommands.deleteOne({ command: command.config.command })
-        await M.reply(`*${this.client.util.capitalize(command.config.command)}* is now Enabled`)
+        await M.reply(`*${this.client.util.capitalize(command.config.command)}* Sijan naba Yaray!`)
     }
 }
