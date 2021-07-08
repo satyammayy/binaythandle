2a19a22a0b6b9f89ade127dd1184cc5f85d7e159
satyammayy <84136113+satyammayy@users.noreply.github.com>
2021-07-08 11:45:16 +0530

    Update pat.ts

--- src/commands/Fun/pat.ts
+++ src/commands/Fun/pat.ts
@@ -1,8 +1,8 @@
-import MessageHandler from '../Handlers/MessageHandler'
-import BaseCommand from '../lib/BaseCommand'
-import { getGifReply } from '../lib/getGify'
-import WAClient from '../lib/WAClient'
-import { IParsedArgs, ISimplifiedMessage } from '../typings'
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import { getGifReply } from '../../lib/getGify'
+import WAClient from '../../lib/WAClient'
+import { IParsedArgs, ISimplifiedMessage } from '../../typings'
 
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
