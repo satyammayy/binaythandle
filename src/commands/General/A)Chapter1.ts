67e7360ae8fe59b49ccb19308691262adcf48966
satyammayy <84136113+satyammayy@users.noreply.github.com>
2021-07-09 21:16:12 +0530

    Update and rename A)Chapter1 to A)Chapter1.ts

--- src/commands/General/A)Chapter1.ts
+++ src/commands/General/A)Chapter1.ts
@@ -20,22 +20,10 @@ export default class Command extends BaseCommand {
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
         M.reply('🎀_*ɴɢᴀᴋ ɴɢᴀɪʙɪʏᴜᴋᴏ......*_*!*🎀')
-        if (!M.urls.length) return void M.reply('Youtube URL ama hapirkoh!')
-        const audio = new YT(M.urls[0], 'audio')
-        if (!audio.validateURL()) return void M.reply(`Please provide a Valid YT URL`)
-        const { videoDetails } = await audio.getInfo()
-        M.reply(
-            await audio.getThumbnail(),
-            MessageType.image,
-            Mimetype.jpeg,
-            undefined,
-            ` Made By *Satyam Mayengbam* \n\n🍥 *Title:* ${videoDetails.title}\n🕰️ *Duration:* ${videoDetails.lengthSeconds}\n🗒️ *Description:* ${videoDetails.description}`
-        )
-        M.reply(await audio.getBuffer(), MessageType.audio).catch(() => M.reply('Yoza!!! Yadiye Try again!'))
-    }
+        M.reply('https://drive.google.com/file/d/1i9vAIm6jIUewzS6_5Bmj-p3TZ-77jheE/view?usp=sharing')
+        
 }
 
 
 
 
-https://drive.google.com/file/d/1i9vAIm6jIUewzS6_5Bmj-p3TZ-77jheE/view?usp=sharing
