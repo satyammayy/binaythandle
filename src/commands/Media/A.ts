673762173a1176e70284befcffbc2715262e0095
satyammayy <84136113+satyammayy@users.noreply.github.com>
2021-07-09 22:38:36 +0530

    Update A.ts

--- src/commands/Media/A.ts
+++ src/commands/Media/A.ts
@@ -8,7 +8,7 @@ import { ISimplifiedMessage } from '../../typings'
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
-            command: 'A',
+            command: 'a',
             description: 'dwnload chp1!',
             category: 'media',
             aliases: ['Chapter1'],
